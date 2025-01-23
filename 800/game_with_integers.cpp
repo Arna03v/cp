df782544fd14635eb9fde03ee4b8be797bd2c8d0
Arnav Gupta <arnvgpta95@gmail.com>
2025-01-23 19:56:12 +0530

    game with integers

---
+++ 800/game_with_integers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+
+
+    if((n-1)%3 == 0 || (n+1)%3 == 0) cout << "First" << endl;
+    else cout <<"Second" << endl;
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--)solve();
+}
