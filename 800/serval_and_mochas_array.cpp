92719b6680d64ee8c76e782cebe8e88124a3f93f
Arna03v <arnvgpta95@gmail.com>
2025-02-19 00:24:43 +0530

    serval and mocha's array

---
+++ 800/serval_and_mochas_array.cpp
@@ -0,0 +1,34 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve(){
+//     int n; cin >> n; 
+//     vector<int> nums(n);
+//     for(int& el : nums) cin >> el;
+
+//     int gcd_value = 1;
+
+//     for(int i = 0; i < n; i++){
+//         for(int j = i+1; j < n; j++){
+//             gcd_value = max(gcd_value, gcd(nums[i], nums[j]));
+            
+//         }
+//     }
+
+//     cout << "gcd is " << gcd_value << endl;
+//     if(gcd_value > n){
+//         cout << "No" << endl;;
+//         return;
+//     }
+//     cout << "Yes" << endl;
+//     return;
+
+// }
+
+// int main(){
+//     int t; cin >> t; 
+//     while(t--) solve();
+//     return 0;
+// }
+
+// TEST CASES ARE WRONG
