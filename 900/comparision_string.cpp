a0bd49e7295d2e2613b3ac8cea0fa86f3797a70b
Arnav Gupta <arnvgpta95@gmail.com>
2025-03-03 18:39:32 +0530

    comparision string

---
+++ 900/comparision_string.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cctype>
+#include <utility>
+using namespace std;
+
+void solve(){
+    int n; cin >> n; 
+    string s; cin >> s;
+
+    // find the longest cons seq of same characters
+    // for <<<<><<<<<
+    // after the '>'; we can just print the element as 1 again and the sequence starts again
+
+    int curr = 1;
+    int res = 1;
+    for(int i=0; i<n-1; i++){
+        if(s[i] == s[i+1]){
+            curr++;
+            res = max(res, curr);
+        }else{
+            curr = 1;
+        }
+    }
+
+    cout << res+1 << endl;
+}
+
+int main(){
+    int t; cin >> t; while(t--) solve();
+}
+
+/*
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n; vector<int> nums(n);
+    for(int& el : nums) cin >> el;
+}
+
+int main(){
+    int t; cin >> t; while(t--) solve();
+}
+*/
