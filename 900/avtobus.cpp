7b9ef6440d4803f4d0e66a4e2738d60fe8c072c5
Arnav Gupta <arnvgpta95@gmail.com>
2025-03-20 16:44:36 +0530

    avtobus working

--- 900/avtobus.cpp
+++ 900/avtobus.cpp
@@ -1,50 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <set>
-#include <unordered_map>
-#include <map>
-#include <unordered_map>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <cctype>
-#include <utility>
-#include <numeric>
+
 using namespace std;
 
 #define nl "\n"
+#define int long long
 
-inline void solve(){
-    int n; cin >> n; 
+inline void solve() {
+    int n;
+    cin >> n;
 
-    // IF N IS ODD THEN ITS NOT POSSIBLE
-    if(n&1 || n==2){
+    // If n is odd or n is 2, there is no valid solution
+    if (n & 1 || n == 2) {
         cout << -1 << nl;
         return;
     }
 
-    // 4x + 6y = n
-    // 2x + 3y = n/2
+    int min_buses = (n + 6 - 1) / 6; // Equivalent to ceil(n/6)
+    int max_buses = n / 4;           // Equivalent to floor(n/4)
 
-    // maximise the number of buses; increase value of x
-    // if n/2 is even (say n = 12)
-    // x = 3
-    // if n/2 is odd (say n=14)
-    // x = 2, y = 1
-    // so we have floor(n/2) buses
-
-    // minimise the buses; increase value of y
-    // 3k, 3k+1, 3k+2
-    // we get ceil(n/3)
-
-    cout << ceil(n/3) << " " << floor(n/2) << nl;
-    return;
-
+    cout << min_buses << " " << max_buses << nl;
 }
 
-int main(){
+signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t; cin >> t; while(t--) solve();
+    int t;
+    cin >> t;
+    while (t--) solve();
 }
