cb7e0ed96e593c41d000ef47060a106c817e619b
Arnav Gupta <arngupta@ea.com>
2025-04-01 15:02:22 +0530

    01 game

--- 900/01_game.cpp
+++ 900/01_game.cpp
@@ -17,8 +17,27 @@ using namespace std;
 #define int long long
 
 void solve(){
-    int n; cin >> n; vector<int> nums(n);
-    for(int& el : nums) cin >> el;
+    string s; cin >> s;
+
+    int one_count = 0;
+    int zero_count = 0;
+
+    for(char& c : s){
+        if(c == '1')one_count++;
+        else zero_count++;
+    }
+
+    // we can play until we have only 1s or only 0s
+    // each move we reduce 1 and 0
+    int number_of_moves = min(one_count, zero_count);
+
+    if(number_of_moves&1){
+        cout << "DA" << nl; return;
+    }else{
+        cout << "NET" << nl; return;
+    }
+
+
 }
 
 signed main(){
